05f1cb07f8110b624aa3b86aa7ed470780f5b8b7
dalilia <dalila200875yahoo.fr>
2023-08-11 23:11:11 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+    const button = document.getElementById('button');
+    
+    button.addEventListener('click', function() {
+        // This function will be executed when the button is clicked
+        console.log('Button clicked!');
+    });
 }
+
+// Don't forget to call the outer function to set up the event listener
+addingEventListener();
